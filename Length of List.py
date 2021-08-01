b41a3d633187e150d330f37283205788803e08e0
Surendra Pratap Dhamala <83567924+Suru996@users.noreply.github.com>
2021-08-01 17:01:38 +0545

    List and Tuples

---
+++ Length of List.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+c = []
+d = []
+for x in range(0,a):
+  e = input()
+  c.append(e)
+for x in range(0,b):
+  e = input()
+  d.append(e)
+print("Length of First List",a)
+print("Length of Second",b)
+print("First List Data")
+for x in range(0,a):
+  print(c[x])
+print("Secind List Data")
+for x in range(0,b):
+  print(d[x])
